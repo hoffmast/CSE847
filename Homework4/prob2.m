8ee30ec1a4b57c2c46f878fd384ba6235fe268da
Steven MW Hoffman <hoffm470@cse.msu.edu>
2016-04-04 18:05:13 -0400

    Updated prob2 and added ROC plot

--- Homework4/prob2.m
+++ Homework4/prob2.m
@@ -33,6 +33,8 @@ par  = [0, 0.0001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
 w = zeros(numel(par),size(data_train,2)); % all weights
 b = zeros(numel(par),1); % all biases
 p = zeros(numel(par), size(data_test,1)); % all predictions
+fars = zeros(numel(par), numel(label_test)+1);
+gars = zeros(numel(par), numel(label_test)+1);
 
 % par = [0.01, 0.02, 0.05, 0.1, 0.2];
 % par = [0.01, 0.02, 0.05, 0.1, 0.2]*0.1;
@@ -70,10 +72,16 @@ for i = 1:numel(par)
 %     cp = classperf(label_test,class_choice_pn1(predictions));
     cp = classperf(label_test >= 0,predictions >=0);
     accuracy(i) = cp.CorrectRate;
-    [~,~,~,auc] = perfcurve(label_test, predictions,1);
+    [far,gar,thres,auc] = perfcurve(label_test, predictions,1);
 %     [~,~,~,auc] = perfcurve(label_test, predictions/max(abs(predictions)),1);
 %     [~,~,~,auc] = perfcurve(label_test, class_choice_pn1(predictions),1);
+%     [~,~,~,auc] = perfcurve(label_test, double(predictions >= 0),1);
+%     [~,~,~,auc] = perfcurve(label_test, sigmf(predictions,[1,0]),1);
     aucs(i) = auc;
+    if numel(far) == size(fars,2)
+       fars(i,:) = far;
+       gars(i,:) = gar;
+    end
 end
 
 %% Plot the results
@@ -100,3 +108,48 @@ plot(par, num_feats_plus_bias, 'o-');
 title('Problem 2: Sparse Logistic Regression');
 xlabel('l_1 regularization parameter');
 ylabel('Number of Features (plus bias)');
+
+% Plot ROC curves
+figure;
+legend_labs = cell(1,size(fars,1));
+j = 1;
+i = 1;
+while j <= 4 && i <= size(fars,1)
+   
+    plot(fars(i,:),gars(i,:),'-o');
+    legend_labs{i} = ['Par = ' num2str(par(i))];
+    hold on;
+    i = i + 1;
+    j = j + 1;
+end
+j = 1;
+while j <= 4 && i <= size(fars,1)
+   
+    plot(fars(i,:),gars(i,:),'-x');
+    legend_labs{i} = ['Par = ' num2str(par(i))];
+    hold on;
+    i = i + 1;
+    j = j + 1;
+end
+j = 1;
+while j <= 4 && i <= size(fars,1)
+   
+    plot(fars(i,:),gars(i,:),'-v');
+    legend_labs{i} = ['Par = ' num2str(par(i))];
+    hold on;
+    i = i + 1;
+    j = j + 1;
+end
+j = 1;
+while j <= 4 && i <= size(fars,1)
+   
+    plot(fars(i,:),gars(i,:),'-^');
+    legend_labs{i} = ['Par = ' num2str(par(i))];
+    hold on;
+    i = i + 1;
+    j = j + 1;
+end
+title('ROCs for various regularization terms');
+xlabel('False Accept Rates (FARs)');
+ylabel('Genuine Accept Rates (GARs)');
+legend(legend_labs);
